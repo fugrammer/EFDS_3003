07161b961cdc1b54de5369cf66296b3f590ebf20
wongsw <wong.songwei@gmail.com>
2017-11-05 16:24:38 +0800

    updated DEPTfire

---
+++ DeptFire/views/orderSquadForm.js
@@ -0,0 +1,24 @@
+$("#orderSQUAD").submit(function(e) {
+    var url = "/HQ/OrderSquad"; // the script where you handle the form input.
+    $.ajax({
+      type: "POST",
+      url: url,
+      data: $("#orderSQUAD").serialize(), // serializes the form's elements.
+      success: function(data) {
+        UIkit.notification({
+          message: "Update sent!",
+          status: "primary",
+          pos: "top-right",
+          timeout: 10000
+        });
+        //alert(JSON.stringify(data)); // show response from the php script.
+        $("#orderSQUAD")[0].reset();
+      },
+      statusCode: {
+        404: function() {
+          alert("Incorrect data entered!");
+        }
+      }
+    });
+    e.preventDefault(); // avoid to execute the actual submit of the form.
+  });
