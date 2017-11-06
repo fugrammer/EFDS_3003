a50a0ad2c6691395df53bf028c3bc5e9ac009632
Thomas Tham <thamtinghoi@gmail.com>
2017-11-06 21:01:44 +0800

    Added 2 other departments

---
+++ DeptBomb/backend/DeptFire.js
@@ -0,0 +1,162 @@
+module.exports = function (io,mongoose,Schemas) {
+  var express = require("express"),
+    app = express(),
+    router = express.Router(),
+    bodyParser = require("body-parser"),
+    urlencodedParser = bodyParser.urlencoded({ extended: false }),
+    jsonParser = bodyParser.json({ limit: "500mb" }),
+    fs = require("fs"),
+    Schema = mongoose.Schema;
+    // Schemas = require("./Schemas");
+
+  router.get("/", function (req, res) {
+    var html = fs.readFileSync(__dirname + "/../views/index.html");
+    res.end(html);
+  });
+
+  /* Provide overall of DeptFire */
+  router.get("/getStatus", function (req, res) {
+    var departmentStatus = {}
+    var filter = {"DepartmentID":"Fire"};
+    Schemas.DepartmentDB.find(filter).lean().exec(function (err, data) {
+      for (var _data of data) {
+        departmentStatus[_data.DepartmentID] = departmentStatus[_data.DepartmentID] || { max: 0, available: 0 };
+        departmentStatus[_data.DepartmentID].max += 1;
+        if (_data.SquadStatus === "Available")
+          departmentStatus[_data.DepartmentID].available += 1;
+      }
+      res.json(departmentStatus);
+    });
+
+  })
+
+  /* When app first loaded */
+  router.get("/getPastOrders", function (req, res) {
+    var filter = {DepartmentID:"DeptFire"}
+    Schemas.DepartmentOrder.find(filter).lean().exec(function (err, data) {
+      res.json(data);
+    });
+  });
+
+  router.get("/getPastUpdates", function (req, res) {
+    var filter = {DepartmentID:"DeptFire"}
+    Schemas.UpdateDept.find(filter).lean().exec(function (err, data) {
+      res.json(data);
+    });
+  });
+  /* TO DO */
+  /* Order below and receive update from below, and receive order from above and update above */
+  router.post("/OrderDept", [urlencodedParser, jsonParser], function (req, res) {
+    var departmentOrder = Schemas.DepartmentOrder({
+      DepartmentID: req.body.DepartmentID,
+      NumberOfSquads: req.body.NumberOfSquads,
+      Lat: req.body.Lat,
+      Lon: req.body.Lon,
+      CrisisID: req.body.CrisisID,
+      Severity: req.body.Severity,
+      Comments: req.body.Comments
+    });
+
+    departmentOrder.save(function (err, dat) {
+      if (err) console.log("Failed to save department order log to department db");
+    });
+    console.log("ReceiveHQOrder");
+    console.log(departmentOrder); 
+    io.emit("ReceiveHQOrder", departmentOrder);
+    res.end("success");
+  });
+
+  router.post("/OrderSquad",[urlencodedParser, jsonParser],function(req,res){
+    var host = req.get('host');
+    console.log(`Host is ${host}`);
+    console.log(JSON.stringify(req.body));
+    department = req.body.DepartmentID;
+    squad = req.body.SquadID;
+    var request=require('request');
+       var json = req.body;
+       var options = {
+         url: 'http://localhost:3000/'+department+"/"+squad,
+         method: 'POST',
+         headers: {
+           'Content-Type': 'application/json'
+         },
+         json: json
+       };
+       request(options, function(err, res, body) {
+         if (res && (res.statusCode === 200 || res.statusCode === 201)) {
+            console.log("Order squad success");
+           console.log(body);
+         }else{
+           console.log("order squad failed")
+         }
+       });
+       res.end("success");
+  })
+
+  router.post("/updateDept", [urlencodedParser, jsonParser], function (req, res) {
+    
+    var updateDept = Schemas.UpdateDept({
+      "DepartmentID":req.body.DepartmentID,
+      "SquadID":req.body.SquadID,
+      "CrisisID": req.body.CrisisID,
+      "Status": req.body.Status,
+      "Comments": req.body.Comments
+    });
+
+    console.log("update dept with: ");
+    console.log(updateDept);
+    newData = {
+      Lat: req.body.Lat,
+      Lon: req.body.Lon,
+      SquadStatus: req.body.Status
+    }
+    /* Share DB with DepartmentDB lazy */
+    /* Change both to update or create */
+    var query = { DepartmentID: req.body.DepartmentID.replace("Dept",""), SquadID:req.body.SquadID}; 
+    Schemas.DepartmentDB.findOneAndUpdate(query, newData, { upsert: true }, function (err, doc) {
+      if (err) {
+        console.log("Failed to save squad update");
+      } else {
+        console.log("Saved squad update");
+      }
+    });
+
+    updateDept.save(function (err, dat) {
+      if (err) console.log("Failed to save updateHQ log");
+    });
+    io.emit("ReceiveSquadUpdates", updateDept);
+    res.json(require("../../Commons/js/response").success);
+  });
+
+  router.post("/updateHQ", urlencodedParser, function (req, res) {
+    console.log("updateHQ");
+    console.log(req.body);
+    var a = {};
+    for (let key of Object.keys(req.body)) {
+      if (!req.body[key]) {
+        res.writeHead(404);
+        res.end();
+        return;
+      }
+    }
+    var request = require('request');
+    var json = req.body;
+    var options = {
+      url: 'http://localhost:3000/HQ/updateHQ',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(req.body)
+    };
+    request(options, function (err, res, body) {
+      if (res && (res.statusCode === 200 || res.statusCode === 201)) {
+        console.log(body);
+      } else {
+        console.log("Failed to send update to HQ"); 
+      }
+    });
+    res.json(require("../../Commons/js/response").success);
+  });
+  return router;
+};
---
+++ DeptHazmat/views/app.js
@@ -0,0 +1,124 @@
+var DeptFireapp = angular.module("DeptFire", ["ngRoute"]);
+angular
+  .module("DeptFire")
+  .controller("ReceiveHQOrdersController", function($scope, socket) {
+    // get past orders when first opened page
+    console.log("DeptFire first opened");
+    $scope.DeptFireOrders = [];
+    $.ajax({
+      type: "GET",
+      url: "/DeptFire/getPastOrders",
+      success: function(data) {
+        console.log(data);
+        for (let data1 of data){
+          $scope.$apply(function(){
+            $scope.DeptFireOrders.push(data1);
+          })
+        }
+      }
+    });
+    // receives new order
+    socket.on("ReceiveHQOrder", function(data) {
+      //$scope.$apply(function() {
+        UIkit.notification({
+          message: "New order received!",
+          status: "primary",
+          pos: "top-right",
+          timeout: 10000
+        });
+      console.log(data);
+      $scope.DeptFireOrders.push(data);
+      //$scope.newCustomers.push(data.customer);
+      // });
+    });
+  });
+
+angular
+  .module("DeptFire")
+  .controller("ReceiveSquadUpdatesController", function($scope, socket) {
+    // get past updates when first opened page
+    console.log("DeptFire first opened");
+    $scope.squadUpdates = [];
+    console.log("making ajax");
+    $.ajax({
+      type: "GET",
+      url: "/DeptFire/getPastUpdates",
+      success: function(data) {
+        console.log(data);
+        for (let data1 of data){
+          $scope.$apply(function(){
+            $scope.squadUpdates.push(data1);
+          })
+        }
+      },
+      failure: function(result){
+        console.log("FAILED");
+        console.log(result);
+    }
+    });
+    console.log("after ajax");
+    // receives new updates
+    socket.on("ReceiveSquadUpdates", function(data) {
+        UIkit.notification({
+          message: "New updates received!",
+          status: "primary",
+          pos: "top-right",
+          timeout: 10000
+        });
+      console.log(`ReceiveSquadUpdates data:`);
+      console.log(data);
+      $scope.squadUpdates.push(data);
+    });
+  });
+
+// boilerplate
+DeptFireapp.factory("socket", function($rootScope) {
+  var socket = io();
+  return {
+    on: function(eventName, callback) {
+      socket.on(eventName, function() {
+        var args = arguments;
+        //console.log(args);
+        $rootScope.$apply(function() {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function(eventName, data, callback) {
+      socket.emit(eventName, data, function() {
+        var args = arguments;
+        $rootScope.$apply(function() {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      });
+    }
+  };
+});
+
+
+// $("#submitUpdate").submit(function(e) {
+//   var url = "/HQ/updateCMO"; // the script where you handle the form input.
+//   $.ajax({
+//     type: "POST",
+//     url: url,
+//     data: $("#submitUpdate").serialize(), // serializes the form's elements.
+//     success: function(data) {
+//       UIkit.notification({
+//         message: "Update sent!",
+//         status: "primary",
+//         pos: "top-right",
+//         timeout: 10000
+//       });
+//       //alert(JSON.stringify(data)); // show response from the php script.
+//       $("#submitUpdate")[0].reset();
+//     },
+//     statusCode: {
+//       404: function() {
+//         alert("Incorrect data entered!");
+//       }
+//     }
+//   });
+//   e.preventDefault(); // avoid to execute the actual submit of the form.
+// });
