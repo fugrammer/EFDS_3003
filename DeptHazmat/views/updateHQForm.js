db9d1e68e5601dc748ff067727b2fb70941ab2bf
XY <wxy888@live.com>
2017-11-09 00:50:34 +0800

    added updateHQForm, invisible input & reference to script

---
+++ DeptHazmat/views/updateHQForm.js
@@ -0,0 +1,25 @@
+$("#updateHQ").submit(function(e) {
+    var url = "/DeptHazmat/updateHQ"; // the script where you handle the form input.
+
+    $.ajax({
+      type: "POST",
+      url: url,
+      data: $("#updateHQ").serialize(), // serializes the form's elements.
+      success: function(data) {
+        UIkit.notification({
+          message: "Update sent!",
+          status: "primary",
+          pos: "top-right",
+          timeout: 10000
+        });
+        //alert(JSON.stringify(data)); // show response from the php script.
+        $("#updateHQ")[0].reset();
+      },
+      statusCode: {
+        404: function() {
+          alert("Incorrect data entered!");
+        }
+      }
+    });
+    e.preventDefault(); // avoid to execute the actual submit of the form.
+  });
